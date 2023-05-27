f7041852fcf830522e8db1b2724fb1293f45f4b2
rod12151 <rodmel.calderon.27@unsch.edu.pe>
2023-05-27 18:13:44 -0500

    add implement bcryptPasswordEncoder

--- src/main/java/com/rodmel/backenduserApp/auth/SpringSecurityConfig.java
+++ src/main/java/com/rodmel/backenduserApp/auth/SpringSecurityConfig.java
@@ -10,13 +10,19 @@ import org.springframework.security.config.annotation.authentication.configurati
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @AllArgsConstructor
 public class SpringSecurityConfig {
     private AuthenticationConfiguration authenticationConfiguration;
-
+    @Bean
+    PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
     @Bean
     SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
 
