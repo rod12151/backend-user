54689ab93b7d43d86b9c27847534744778276217
rod12151 <rodmel.calderon.27@unsch.edu.pe>
2023-05-27 08:16:12 -0500

    add filter JwtAuthentication in SpringSecurity config

--- src/main/java/com/rodmel/backenduserApp/auth/SpringSecurityConfig.java
+++ src/main/java/com/rodmel/backenduserApp/auth/SpringSecurityConfig.java
@@ -1,22 +1,31 @@
 package com.rodmel.backenduserApp.auth;
 
+import com.rodmel.backenduserApp.auth.filters.JwtAuthenticationFilter;
+import lombok.AllArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
+@AllArgsConstructor
 public class SpringSecurityConfig {
+    private AuthenticationConfiguration authenticationConfiguration;
+
     @Bean
     SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
+
          http
                 .authorizeHttpRequests()
                 .requestMatchers(HttpMethod.GET,"/users").permitAll()
                 .anyRequest().authenticated()
                 .and()
+                .addFilter(new JwtAuthenticationFilter(authenticationConfiguration.getAuthenticationManager()))
                 .csrf()
                 .disable()
                 .sessionManagement()
