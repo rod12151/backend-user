209135db1571c30e0c7239c95c3d4110fc5c6576
rod12151 <rodmel.calderon.27@unsch.edu.pe>
2023-05-27 06:09:10 -0500

    validaciones en entity

--- src/main/java/com/rodmel/backenduserApp/models/entities/User.java
+++ src/main/java/com/rodmel/backenduserApp/models/entities/User.java
@@ -1,6 +1,9 @@
 package com.rodmel.backenduserApp.models.entities;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotEmpty;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -15,9 +18,15 @@ public class User {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     @Column(unique = true)
+    @NotBlank
     private String username;
+
+    @NotEmpty
     private String password;
+
     @Column(unique = true)
+    @NotEmpty
+    @Email
     private String email;
 
 }
