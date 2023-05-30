36e8c5060688f08f7a33c4f1ba6345e16720df85
rod12151 <rodmel.calderon.27@unsch.edu.pe>
2023-05-29 22:21:38 -0500

    validando token jwt

--- src/main/java/com/rodmel/backenduserApp/auth/filters/JwtValidationFilter.java
+++ src/main/java/com/rodmel/backenduserApp/auth/filters/JwtValidationFilter.java
@@ -1,12 +1,15 @@
 package com.rodmel.backenduserApp.auth.filters;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.rodmel.backenduserApp.auth.TokenJwtConfig;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.JwtException;
+import io.jsonwebtoken.Jwts;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-import org.apache.el.parser.Token;
+
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.GrantedAuthority;
@@ -30,19 +33,20 @@ public class JwtValidationFilter extends BasicAuthenticationFilter {
             FilterChain chain)
             throws IOException, ServletException {
         String header = request.getHeader(HEADER_AUTHORIZATION);
+
         if(header == null || !header.startsWith((PREFIX_TOKEN))){
             chain.doFilter(request,response);
             return;
         }
         String token = header.replace(PREFIX_TOKEN,"");
-        byte[] tokenDecodeBytes = Base64.getDecoder().decode(token);
-        String tokenDecode = new String(tokenDecodeBytes);
 
-        String[] tokenArr = tokenDecode.split(":");
-        String secret = tokenArr[0];
-        String username = tokenArr[1];
-
-        if(SECRET_KEY.equals(secret)){
+        try{
+            Claims claims= Jwts.parserBuilder()
+                    .setSigningKey(SECRET_KEY)
+                    .build()
+                    .parseClaimsJws(token)
+                    .getBody();
+            String username = claims.getSubject();
             List<GrantedAuthority> authorities = new ArrayList<>();
             authorities.add(new SimpleGrantedAuthority("ROLE_USER"));
 
@@ -50,8 +54,9 @@ public class JwtValidationFilter extends BasicAuthenticationFilter {
                     authorities);
             SecurityContextHolder.getContext().setAuthentication(authentication);
             chain.doFilter(request,response);
-        }else {
+        }catch (JwtException e){
             Map<String,String> body = new HashMap<>();
+            body.put("error",e.getMessage());
             body.put("mensaje","El token JWT no es valido!");
 
             response.getWriter().write(new ObjectMapper().writeValueAsString(body));
