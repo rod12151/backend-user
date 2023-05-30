3b9afff9b3d1ad50c42b6b477b97b29f81692c09
rod12151 <rodmel.calderon.27@unsch.edu.pe>
2023-05-29 19:56:07 -0500

    login con jpa

--- src/main/java/com/rodmel/backenduserApp/repositories/UserRepository.java
+++ src/main/java/com/rodmel/backenduserApp/repositories/UserRepository.java
@@ -2,7 +2,14 @@ package com.rodmel.backenduserApp.repositories;
 
 import com.rodmel.backenduserApp.models.entities.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User,Long> {
 
+    Optional<User> findByUsername(String username);
+
+/*    @Query("select u from User  u where u.username=?1")
+    Optional<User> getUserByUsername(String username);*/
 }
--- src/main/java/com/rodmel/backenduserApp/services/JpaUserDetailsService.java
+++ src/main/java/com/rodmel/backenduserApp/services/JpaUserDetailsService.java
@@ -1,5 +1,7 @@
 package com.rodmel.backenduserApp.services;
 
+import com.rodmel.backenduserApp.repositories.UserRepository;
+import lombok.AllArgsConstructor;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.User;
@@ -7,25 +9,37 @@ import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
+
 @Service
+@Transactional(readOnly = true)
+@AllArgsConstructor
 public class JpaUserDetailsService implements UserDetailsService {
+    private UserRepository userRepository;
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        if(!username.equals("admin")){
-            throw new UsernameNotFoundException(String.format("Username%s no existe en el sistema!",username));
+        Optional<com.rodmel.backenduserApp.models.entities.User> o= userRepository.findByUsername(username);
+
+        if(o.isPresent()){
+            com.rodmel.backenduserApp.models.entities.User user = o.orElseThrow();
+            List<GrantedAuthority> authorities = new ArrayList<>();
+            authorities.add(new SimpleGrantedAuthority("ROLE_USER"));
+            return new User(
+                    user.getUsername(),
+                    user.getPassword(),
+                    true,
+                    true,
+                    true,
+                    true,
+                    authorities);
 
         }
-        List<GrantedAuthority> authorities = new ArrayList<>();
-        authorities.add(new SimpleGrantedAuthority("ROLE_USER"));
-        return new User(username,
-                "$2a$10$DOMDxjYyfZ/e7RcBfUpzqeaCs8pLgcizuiQWXPkU35nOhZlFcE9MS",
-                true,
-                true,
-                true,
-                true,
-                authorities);
+        throw new UsernameNotFoundException(String.format("Username%s no existe en el sistema!",username));
+
+
     }
 }
